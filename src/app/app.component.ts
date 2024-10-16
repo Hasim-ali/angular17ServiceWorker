4c929f1aab005b2ca7b8a3d6f2a8f0344953f7c1
Hasim <shasim148@gmail.com>
2024-10-16 11:08:07 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwPush, SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,48 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'test';
+  constructor(swUpdate: SwUpdate, snackBar: MatSnackBar, swPush: SwPush) {
+
+    // commently check when we load the web site
+    swUpdate.versionUpdates.subscribe(() => {
+      const snack = snackBar.open('New version available', 'Reload');
+      snack.onAction().subscribe(() => {
+        swUpdate.activateUpdate().then(() => window.location.reload());
+      });
+    });
+
+    // commently check when we load the web site and reload the web site when new version is avaliable and ready
+    swUpdate.versionUpdates.
+      pipe(filter(
+        (event): event is VersionReadyEvent => event.type === 'VERSION_READY'
+      ))
+      .subscribe(() => {
+        const snack = snackBar.open('New version available', 'Reload');
+        snack.onAction().subscribe(() => {
+          swUpdate.activateUpdate().then(() => window.location.reload());
+        });
+      });
+
+    // commently check when we load the web site and reload the web site when new version is avaliable with dynamically
+    swUpdate.versionUpdates.subscribe((evt) => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          swUpdate.activateUpdate().then(() => {
+            console.log('Update activated successfully. Reloading the page.');
+            document.location.reload();
+          });
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+        default:
+          // console.log("default case running")
+      }
+    });
+  }
 }
